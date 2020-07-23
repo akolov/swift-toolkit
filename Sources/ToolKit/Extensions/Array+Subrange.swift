ac50f1701fc64ba8f4d32fa826e11c1037649e49
Alexander Kolov <me@alexkolov.com>
2020-07-23 15:34:55 +0900

    Safe subrange

---
+++ Sources/ToolKit/Extensions/Array+Subrange.swift
@@ -0,0 +1,21 @@
+//
+//  Array+Subrange.swift
+//  ToolKit
+//
+//  Created by Alexander Kolov on 2020-07-23.
+//  Copyright © 2020 Alexander Kolov. All rights reserved.
+//
+
+extension Array {
+
+  public func subrange(in range: Range<Index>) -> ArraySlice<Element> {
+    guard indices.contains(range.lowerBound) else {
+      return self[0..<0]
+    }
+
+    let lower = range.lowerBound
+    let upper = Swift.min(range.upperBound, indices.upperBound)
+    return self[lower..<upper]
+  }
+
+}
