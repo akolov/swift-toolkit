cae50b57a4d46739910385904eb5653ca2d0f960
Alexander Kolov <me@alexkolov.com>
2021-02-17 17:45:19 +0900

    Rename package and bump swift tools version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "ToolKit",
+  name: "swift-toolkit",
   platforms: [
     .macOS(.v10_13), .iOS(.v12), .tvOS(.v12), .watchOS(.v5)
   ],
